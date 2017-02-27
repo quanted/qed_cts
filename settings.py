7c3565dce980fb3ae4f7233890381ada05006247
Tom Purucker <purucker.tom@gmail.com>
2017-02-27 18:40:08 +0000

    urls and settings never deployed

--- settings.py
+++ settings.py
@@ -75,11 +75,17 @@ INSTALLED_APPS = (
     # 'rest_framework_swagger',
     'cts_app',  # cts django app
     'cts_app.filters',  # cts filters for pchem table
-    'cts_app.cts_testing',
     'cts_app.cts_api',
-    'splash_app',
-    'ubertool_app',
-    'hwbi_app',
+    'cts_app.cts_testing',
+    'cyan_app',  # cyan django app
+    'hem_app',  # hem django app
+    'hms_app',  # hms django app
+    'hwbi_app',  # hwbi django app
+    'pisces_app',  # pisces django app
+    'pop_app',  # pop django app
+    'sam_app',  # sam django app
+    'splash_app',  # splash django app
+    'ubertool_app',  # ubertool django app
 )
 
 # This breaks the pattern of a "pluggable" TEST_CTS django app, but it also makes it convenient to describe the server hosting the TEST API.
