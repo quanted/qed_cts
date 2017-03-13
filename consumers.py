dbcd81c0c6deed2e4c76575dcb176f3be29110c5
popenc <i.nickpope@gmail.com>
2017-03-13 16:55:30 -0400

    some branching updates

--- consumers.py
+++ consumers.py
@@ -3,18 +3,18 @@
 # In consumers.py
 import logging
 from channels import Group
-
 from channels.generic.websockets import JsonWebsocketConsumer
 import os
 import time
 import json
 
-# from cts_app.cts_calcs import smilesfilter  # adding this including only jchem_rest and jchem_calculator from chemaxon_cts... what does that even mean??
-# from cts_app.cts_calcs import data_walks
-from cts_app.cts_calcs import chemaxon_cts
-# from cts_app.cts_calcs import smilesfilter, data_walks, calculator  # adding this including only jchem_rest and jchem_calculator from chemaxon_cts... what does that even mean??
+from cts_app import cts_calcs
+from cts_app.cts_calcs import worker_chemaxon
+from cts_app.cts_calcs import calculator
+# from cts_app.cts_calcs import chemaxon_cts
+# from cts_app.cts_calcs.chemaxon_cts import worker
 
-logging.warning("CHEMAXON DIR: {}".format(dir(chemaxon_cts)))
+logging.warning("CHEMAXON DIR: {}".format(dir(cts_calcs)))
 
 
 def ws_message(message):
@@ -36,7 +36,7 @@ def ws_disconnect(message):
     Group("chat").discard(message.reply_channel)
 
 
-def ws_request_consumer(message, service):
+def ws_request_consumer(message):
     """
     message type from django channels
     service - calc name for p-chem data, metabolizer, speciation
@@ -45,52 +45,58 @@ def ws_request_consumer(message, service):
     logging.info("incoming message to channels channel: {}".format(message))
     post_request = message.content  # expecting json request for channels pchem data
 
-    logging.warning("arg: {}".format(service))
-    logging.warning("message: {}".format(message.content))
+    # logging.info("arg: {}".format(service))
+    logging.info("message: {}".format(message.content))
 
     # get sessionid:
     sessionid = message.content['reply_channel'].split('!')[1]  # ID after "!" in reply ch.
-    logging.warning("sessionid: {}".format(sessionid))
-
+    logging.info("sessionid: {}".format(sessionid))
 
     service_request = json.loads(message.content['text'])  # incoming json string
 
-    # calc_obj = calculator.Calculator()
-    # calc_obj.process_ws_request(service_request)  # populates calc_obj.calc_request
-    # logging.warning("REQUEST OBJ: {}".format(calc_obj.calc_request))
+    # request_handler(sessionid, service_request)
+    request_handler(sessionid, message)
+
+    # calc = service_request['calc']  # calc name, speciation, or transformation products
+
+    # calc_obj = calculator.Calculator(calc)  # if service recognized, get sub class instance
+    # response_dict = calc_obj.request_logic(service_request, message)
+
+    # message.reply_channel.send({'text': json.dumps(response_dict)})  # push to client
 
 
-    # before trying the full pchem request, try a hardcoded one for chemaxon:
+def request_handler(sessionid, data_obj):
 
-    message.reply_channel.send({'text': ''})
-
-    # user_jobs = []  # still track user job IDs?
-    # job_id = pchem_request_handler(sessionid, calc_obj.calc_request)
-
-    # pchem_response = channels_cts.worker.request_manager({'this': 'is', 'not': 'real'})
-    message.reply_channel.send({'text': 'channels worker made p-chem request to {}'.format(service)})
-
-
-def pchem_request_handler(sessionid, data_obj):
-
-    if 'cancel' in message.content['text']:
+    if 'cancel' in data_obj:
         # still need this remove user jobs from queue condition?
         logging.warning("cancel request received at consumers.py")
         return
 
     user_jobs = []  # still track user job IDs?
 
-    if 'nodes' in message.content['text']:
+    if 'nodes' in data_obj:
         for node in data_obj['nodes']:
             node_obj = data_obj['nodes'][node]
             data_obj['node'] = node_obj
             data_obj['chemical'] = node_obj['smiles']
-            jobID = parse_pchem_request(sessionid, data_obj)
+            job_id = parse_request(sessionid, data_obj, message)
     else:
-        jobID = parse_pchem_request(sessionid, data_obj)
+        job_id = parse_request(sessionid, data_obj, message)
 
 
-def parse_pchem_request(sessionid, data_obj):
+def parse_request(sessionid, data_obj, message):
+
+    if data_obj['service'] == 'getSpeciationData' or data_obj['service'] == 'getTransProducts'
+        data_obj['sessionid'] = sessionid
+        # client.call('tasks.chemaxonTask', [data_obj])
+        response_dict = calc_obj.request_logic(data_obj, message)
+        return sessionid
+    else:
+        parse_pchem_request(sessionid, data_obj, client)
+        return sessionid
+
+
+def parse_pchem_request(sessionid, data_obj, message):
     """
     python version of cts_nodejs's node_server.js
     function: pchemRequestHandler
@@ -98,99 +104,13 @@ def parse_pchem_request(sessionid, data_obj):
     for calc_name, props_list in data_obj['pchem_request'].items():
 
         data_obj['calc'] = calc_name
-        # data_obj['props'] = data_obj['pchem_request'][calc_name]
         data_obj['props'] = props_list
         data_obj['sessionid'] = sessionid
 
-        if calc_name == 'chemaxon':
-            # client.call('tasks.chemaxonTask', [data_obj])
-            # make request to calc worker?
-            pass
-        elif calc_name == 'sparc':
-            # client.call('tasks.sparcTask', [data_obj]);   
-            # make request to calc worker?
-            pass
-        elif calc_name == 'epi':
-            # client.call('tasks.epiTask', [data_obj]);   
-            # make request to calc worker?
-            pass
-        elif calc_name == 'test':
-            # client.call('tasks.testTask', [data_obj]);   
-            # make request to calc worker?
-            pass
-        elif calc_name == 'measured':
-            # client.call('tasks.measuredTask', [data_obj]);   
-            # make request to calc worker?
-            pass
+        calc_obj = calculator.Calculator(calc_name)  # get calc-specific class object
 
-    return sessionid;
+        response_dict = calc_obj.request_logic(data_obj, message)  # parsed request sent to calc server(s)
 
+        # message.reply_channel.send({'text': json.dumps(response_dict)})  # push to client
 
-# def ws_pchem_request(message):
-#   # assuming p-chem request from single user.
-#   # parse out request to workers..
-#   check_mess = message;
-#   message.reply_channel.send({
-#       # "text": message.content['text'],
-#       "text": "hello, this is pchem channel",
-#   })
-
-
-# def chemaxon_channel(message):
-#     logging.info("incoming message to chemaxon channel: {}".format(message))
-#     post_request = message.content  # expecting json request for chemaxon pchem data
-#     # return chemaxon_worker.request_manager(request)
-
-#     # this is where the call would go to cts_api rest endpoint
-
-
-#     # pchem_response = chemaxon_cts.worker.request_manager({'this': 'is', 'not': 'real'})
-#     message.reply_channel.send({'text': 'chemaxon worker made p-chem request'})
-
-
-# def sparc_channel(message):
-#     logging.info("incoming message to sparc channel: {}".format(message))
-#     post_request = message.content  # expecting json request for sparc pchem data
-#     # return sparc_worker.request_manager(request)
-
-#     # this is where the call would go to cts_api rest endpoint
-
-
-#     # pchem_response = sparc_cts.worker.request_manager({'this': 'is', 'not': 'real'})
-#     message.reply_channel.send({'text': 'sparc worker made p-chem request'})
-
-
-# # Generic Consumer: JSON-enabled WebSocket Class
-# class ChemaxonConsumer(JsonWebsocketConsumer):
-
-#     # set to True for ordering:
-#     strict_ordering = False
-
-#     method_mapping = {
-#         'channel_chemaxon': 'method name'
-#     }
-
-#     def connection_groups(self, **kwargs):
-#         """
-#         Called to return the list of groups to automatically add/remove
-#         this connection to/from.
-#         """
-#         return ["test"]
-
-#     def connect(self, **kwargs):
-#         """
-#         Perform things on connection start
-#         """
-#         pass
-
-#     def receive(self, content, **kwargs):
-#         """
-#         Called when a message is received with decoded JSON content
-#         """
-#         self.send(content)  # simple echo (self.send auto encodes JSON)
-
-#     def disconnect(self, message, **kwargs):
-#         """
-#         Perform things on connection close
-#         """
-#         pass
+    return sessionid
--- routing.py
+++ routing.py
@@ -1,8 +1,5 @@
 from channels.routing import route, route_class
-# from cts_channels.consumers import ws_add, ws_message, ws_disconnect, ws_pchem_request, chemaxon_channel
-# from cts_channels import consumers
-# from cts_channels.consumers import ws_add, ws_disconnect, ws_request_consumer
-from .consumers import ws_add, ws_disconnect, ws_request_consumer
+from consumers import ws_add, ws_disconnect, ws_request_consumer
 
 channel_routing = [
     route("websocket.connect", ws_add),
